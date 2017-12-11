c3227af26f40b4d9d8f0affce3575793b4d476e7
bradley chan <bradley-chanzh@hotmail.com>
2017-12-11 11:22:31 +0800

    complete assignment-1

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
-
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
 
 app.use(express.static(__dirname + '/public'));
 
